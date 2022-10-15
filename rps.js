8eb96627c24e386527860d66b1fd9bf8820bfe63
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-10-15 17:10:36 -0400

    add main game loop and various checks

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 const totalRounds = 5;
-let numberOfRounds = "1";
 let computerWins = 0;
 let playerWins = 0;
+let noWinner = false;
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3)
@@ -18,88 +18,133 @@ function getComputerChoice() {
     }
     return randomChoice;
 }
+
 function getPlayerChoice() {
-    let PlayerSelection = prompt("Make your choice: Rock, Paper, or Scissors?");
-    PlayerSelection = PlayerSelection.charAt(0).toUpperCase() + PlayerSelection.slice(1).toLowerCase()
-    if (inputIsValid(PlayerSelection)) {
-        return PlayerSelection
+    let playerSelection = prompt("Make your choice: Rock, Paper, or Scissors?");
+    if(playerSelection === null) {
+        alert(`You've got to pick something!`);
+        playerSelection = getPlayerChoice();
+        return playerSelection;
+    }
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (inputIsValid(playerSelection)) {
+        return playerSelection;
     } else {
         alert("Whoops. That's not a valid option")
-        PlayerSelection = getPlayerChoice();
-        return PlayerSelection;
+        playerSelection = getPlayerChoice();
+        return playerSelection;
     }
 }
 
 function inputIsValid(input) {
     if(input === "Rock" || input === "Paper" || input === "Scissors") {
-        return true
+        return true;
     } else {
         return false;
     }
 }
 
 function lostRound(computerChoice, playerChoice) {
-    alert(`You lose this round! ${computerChoice} beats ${playerChoice}.`)
+    alert(`You lose this round! ${computerChoice} beats ${playerChoice}.`);
     computerWins++;
 }
 
 function wonRound(computerChoice, playerChoice) {
-    alert(`You win this round! ${playerChoice} beats ${computerChoice}.`)
+    alert(`You win this round! ${playerChoice} beats ${computerChoice}.`);
     playerWins++;
 }
 
-function tieRound(computerChoice, playerChoice) {
-    alert(`A Tie! You both picked ${computerChoice}.`)
+function tieRound(computerChoice) {
+    alert(`A Tie! You both picked ${computerChoice}.`);
 }
 
-function evaulateWinner(computerChoice, playerChoice) {
+function evaulateRoundWinner(computerChoice, playerChoice) {
     if (computerChoice === "Rock") {
         switch(playerChoice) {
             case "Rock":
-                tieRound(computerChoice, playerChoice);
+                tieRound(computerChoice);
                 return "Tie";
             case "Paper":
                 wonRound(computerChoice, playerChoice);
-                return "Win"
+                return "Win";
             case "Scissors":
                 lostRound(computerChoice, playerChoice);
-                return "Loss"                
+                return "Loss";           
         }
     } else if (computerChoice === "Paper") {
         switch(playerChoice) {
             case "Paper":
-                tieRound(computerChoice, playerChoice);
+                tieRound(computerChoice);
                 return "Tie";
             case "Scissors":
                 wonRound(computerChoice, playerChoice);
-                return "Win"
+                return "Win";
             case "Rock":
                 lostRound(computerChoice, playerChoice);
-                return "Loss"                
+                return "Loss";            
         } 
     } else {
                 switch(playerChoice) {
                     case "Scissors":
-                        tieRound(computerChoice, playerChoice);
+                        tieRound(computerChoice);
                         return "Tie";
                     case "Rock":
                         wonRound(computerChoice, playerChoice);
-                        return "Win"
+                        return "Win";
                     case "Paper":
                         lostRound(computerChoice, playerChoice);
-                        return "Loss"                
+                        return "Loss";            
                 }
         }
     }
 
-function commenceRound(numberOfRounds) {
+
+
+function commenceRound() {
     let computerChoice = getComputerChoice();
     let playerChoice = getPlayerChoice();
-    evaulateWinner(computerChoice, playerChoice);
+    evaulateRoundWinner(computerChoice, playerChoice);
 } 
 
-for (; numberOfRounds <= 5; numberOfRounds++ ) {
-commenceRound();
-alert(`Current Score: \nPlayer: ${playerWins}\nComputer ${computerWins}\n\n${numberOfRounds} out of ${totalRounds} played.`);
+function haveWinner(numberOfRounds) {
+if (numberOfRounds === 5 || playerWins >= 3 || computerWins >= 3) {
+    return true;
+} else {
+    return false;
+}
 }
 
+function playAgainPrompt(victor) {
+        let playAgain = confirm(`The ${victor} wins! Play again?`);
+        if (playAgain) {
+            newGame();
+            } else {
+                 alert(`Thanks for playing!`);
+            }
+}
+
+
+function newGame() {
+    for (let numberOfRounds = 1; numberOfRounds <= totalRounds; numberOfRounds++ ) {
+            commenceRound();
+            alert(`Current Score: \nPlayer: ${playerWins}\nComputer ${computerWins}\n\n${numberOfRounds} out of ${totalRounds} played.`);
+            gameHasWinner = haveWinner(numberOfRounds);
+            if (gameHasWinner) {
+                if (playerWins > computerWins) {
+                    let victor = "player";
+                    playAgainPrompt(victor);
+                } else if (computerWins > playerWins) {
+                    let victor = "computer";
+                    playAgainPrompt(victor);
+                } else {
+                    let victor = "tie";
+                    playAgainPrompt(victor);
+                }
+                return;
+            }
+        }
+    }
+
+
+
+newGame();
